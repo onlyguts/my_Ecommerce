01f7fede76333a926762fc84f98886cdc68016c7
Tony <tony.brechard@epitech.eu>
2024-07-18 16:48:24 +0200

    [ADD] - on commence le CRUD

--- client/src/App/Admin/Admin.js
+++ client/src/App/Admin/Admin.js
@@ -1,185 +1,220 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import AdminCss from './Admin.css'
-import AdminnAV from './NavAdmin'
 import { useNavigate } from "react-router-dom";
-
+import localhost from '../Config';
 function Admin() {
+    const [change, setChange] = useState(true);
     const navigate = useNavigate();
 
-    const change = true
 
     const handleClick = () => {
         navigate("/");
     }
-  return (
 
-    <div class="container">
-        <div class="sidebar">
-            <a onClick={handleClick} class="menu-home">
-                <img src="https://img.icons8.com/ios/50/null/home.png" alt="Dashboard icon" />
-            </a>
-            <details>
-                <summary>
-                    <img src="https://img.icons8.com/ios/50/null/user.png" alt="User icon" />
-                    Users
-                </summary>
-                <div class="container-div">
-                    <a href="#">
-                        <img src="https://img.icons8.com/ios/50/null/user.png" alt="Dashboard icon" />
-                        List All Users
+    const Boitier = () => {
+        setChange(false)
+    }
+    return (
 
-                    </a>
-                    <a href="#">
-                        <img src="https://img.icons8.com/ios/50/null/user.png" alt="Dashboard icon" />
-                        Group Users
-                    </a>
-                    <a href="#">
-                        <img src="https://img.icons8.com/ios/50/null/user.png" alt="Dashboard icon" />
-                        Create a User
-                    </a>
-                    <a href="#">
-                        <img src="https://img.icons8.com/ios/50/null/user.png" alt="Dashboard icon" />
-                        Delete a User
-                    </a>
-                </div>
-            </details>
-            <details>
-                <summary>
-                    <img src="https://img.icons8.com/ios/50/null/shopping-cart.png" alt="User icon" />
-                    Shop
+        <div class="container">
+            <div class="sidebar">
+                <a onClick={handleClick} class="menu-home">
+                    <img src="https://img.icons8.com/ios/50/null/home.png" alt="Dashboard icon" />
+                </a>
+                <details>
+                    <summary>
+                        <img src="https://img.icons8.com/ios/50/null/user.png" alt="User icon" />
+                        Users
+                    </summary>
+                    <div class="container-div">
+                        <a href="#">
+                            <img src="https://img.icons8.com/ios/50/null/user.png" alt="Dashboard icon" />
+                            List All Users
 
-                </summary>
-                <div class="container-div">
-                    <a href="#">
-                        <img src="https://img.icons8.com/ios/50/null/shopping-cart.png" alt="Dashboard icon" />
-                        Change Password
-                    </a>
-                    <a href="#">
-                        <img src="https://img.icons8.com/ios/50/null/shopping-cart.png" alt="Dashboard icon" />
-                        Edit Profile
-                    </a>
-                    <a href="#">
-                        <img src="https://img.icons8.com/ios/50/null/shopping-cart.png" alt="Dashboard icon" />
-                        Notification Settings
-                    </a>
-                    <a href="#">
-                        <img src="https://img.icons8.com/ios/50/null/shopping-cart.png" alt="Dashboard icon" />
-                        Logout
-                    </a>
-                </div>
-            </details>
-            <details>
-                <summary>
-                    <img src="https://img.icons8.com/ios/50/null/data-configuration.png" alt="User icon" />
-                    Data
-                </summary>
-                <div class="container-div">
-                    <a href="#">
-                        <img src="https://img.icons8.com/ios/50/null/data-configuration.png" alt="Dashboard icon" />
-                        Change Password
-                    </a>
-                    <a href="#">
-                        <img src="https://img.icons8.com/ios/50/null/data-configuration.png" alt="Dashboard icon" />
-                        Edit Profile
-                    </a>
-                    <a href="#">
-                        <img src="https://img.icons8.com/ios/50/null/data-configuration.png" alt="Dashboard icon" />
-                        Notification Settings
-                    </a>
-                    <a href="#">
-                        <img src="https://img.icons8.com/ios/50/null/data-configuration.png" alt="Dashboard icon" />
-                        Logout
-                    </a>
-                </div>
-            </details>
-            <details>
-                <summary>
-                    <img src="https://img.icons8.com/ios/50/null/user-shield.png" alt="User icon" />
-                    Admin
-                </summary>
-                <div class="container-div">
-                    <a href="#">
-                        <img src="https://img.icons8.com/ios/50/null/user-shield.png" alt="Dashboard icon" />
-                        Change Password
-                    </a>
-                    <a href="#">
-                        <img src="https://img.icons8.com/ios/50/null/user-shield.png" alt="Dashboard icon" />
-                        Edit Profile
-                    </a>
-                    <a href="#">
-                        <img src="https://img.icons8.com/ios/50/null/user-shield.png" alt="Dashboard icon" />
-                        Notification Settings
-                    </a>
-                    <a href="#">
-                        <img src="https://img.icons8.com/ios/50/null/user-shield.png" alt="Dashboard icon" />
-                        Logout
-                    </a>
-                </div>
-            </details>
-            <details>
-                <summary>
-                    <img src="https://img.icons8.com/ios/50/null/log.png" alt="User icon" />
-                    Log
-                </summary>
-                <div class="container-div">
-                    <a href="#">
-                        <img src="https://img.icons8.com/ios/50/null/log.png" alt="Dashboard icon" />
-                        Change Password
-                    </a>
-                    <a href="#">
-                        <img src="https://img.icons8.com/ios/50/null/log.png" alt="Dashboard icon" />
-                        Edit Profile
-                    </a>
-                    <a href="#">
-                        <img src="https://img.icons8.com/ios/50/null/log.png" alt="Dashboard icon" />
-                        Notification Settings
-                    </a>
-                    <a href="#">
-                        <img src="https://img.icons8.com/ios/50/null/log.png" alt="Dashboard icon" />
-                        Logout
-                    </a>
-                </div>
-            </details>
+                        </a>
+                        <a href="#">
+                            <img src="https://img.icons8.com/ios/50/null/user.png" alt="Dashboard icon" />
+                            Group Users
+                        </a>
+                        <a href="#">
+                            <img src="https://img.icons8.com/ios/50/null/user.png" alt="Dashboard icon" />
+                            Create a User
+                        </a>
+                        <a href="#">
+                            <img src="https://img.icons8.com/ios/50/null/user.png" alt="Dashboard icon" />
+                            Delete a User
+                        </a>
+                    </div>
+                </details>
+                <details>
+                    <summary>
+                        <img src="https://img.icons8.com/ios/50/null/shopping-cart.png" alt="User icon" />
+                        Shop
 
-            <details>
-                <summary>
-                    <img src="https://img.icons8.com/ios/50/null/settings.png" alt="User icon" />
-                    Settings
-                </summary>
-                <div class="container-div">
-                    <a href="#">
-                        <img src="https://img.icons8.com/ios/50/null/settings.png" alt="Dashboard icon" />
-                        Change Password
-                    </a>
-                    <a href="#">
-                        <img src="https://img.icons8.com/ios/50/null/settings.png" alt="Dashboard icon" />
-                        Edit Profile
-                    </a>
-                    <a href="#">
-                        <img src="https://img.icons8.com/ios/50/null/settings.png" alt="Dashboard icon" />
-                        Notification Settings
-                    </a>
-                    <a href="#">
-                        <img src="https://img.icons8.com/ios/50/null/settings.png" alt="Dashboard icon" />
-                        Logout
-                    </a>
-                </div>
-            </details>
+                    </summary>
+                    <div class="container-div">
+                        <a onClick={Boitier}>
+                            <img src="https://img.icons8.com/ios/50/null/shopping-cart.png" alt="Dashboard icon" />
+                            Boitier
+                        </a>
+
+                    </div>
+                </details>
+                <details>
+                    <summary>
+                        <img src="https://img.icons8.com/ios/50/null/data-configuration.png" alt="User icon" />
+                        Data
+                    </summary>
+                    <div class="container-div">
+                        <a href="#">
+                            <img src="https://img.icons8.com/ios/50/null/data-configuration.png" alt="Dashboard icon" />
+                            Change Password
+                        </a>
+                        <a href="#">
+                            <img src="https://img.icons8.com/ios/50/null/data-configuration.png" alt="Dashboard icon" />
+                            Edit Profile
+                        </a>
+                        <a href="#">
+                            <img src="https://img.icons8.com/ios/50/null/data-configuration.png" alt="Dashboard icon" />
+                            Notification Settings
+                        </a>
+                        <a href="#">
+                            <img src="https://img.icons8.com/ios/50/null/data-configuration.png" alt="Dashboard icon" />
+                            Logout
+                        </a>
+                    </div>
+                </details>
+                <details>
+                    <summary>
+                        <img src="https://img.icons8.com/ios/50/null/user-shield.png" alt="User icon" />
+                        Admin
+                    </summary>
+                    <div class="container-div">
+                        <a href="#">
+                            <img src="https://img.icons8.com/ios/50/null/user-shield.png" alt="Dashboard icon" />
+                            Change Password
+                        </a>
+                        <a href="#">
+                            <img src="https://img.icons8.com/ios/50/null/user-shield.png" alt="Dashboard icon" />
+                            Edit Profile
+                        </a>
+                        <a href="#">
+                            <img src="https://img.icons8.com/ios/50/null/user-shield.png" alt="Dashboard icon" />
+                            Notification Settings
+                        </a>
+                        <a href="#">
+                            <img src="https://img.icons8.com/ios/50/null/user-shield.png" alt="Dashboard icon" />
+                            Logout
+                        </a>
+                    </div>
+                </details>
+                <details>
+                    <summary>
+                        <img src="https://img.icons8.com/ios/50/null/log.png" alt="User icon" />
+                        Log
+                    </summary>
+                    <div class="container-div">
+                        <a href="#">
+                            <img src="https://img.icons8.com/ios/50/null/log.png" alt="Dashboard icon" />
+                            Change Password
+                        </a>
+                        <a href="#">
+                            <img src="https://img.icons8.com/ios/50/null/log.png" alt="Dashboard icon" />
+                            Edit Profile
+                        </a>
+                        <a href="#">
+                            <img src="https://img.icons8.com/ios/50/null/log.png" alt="Dashboard icon" />
+                            Notification Settings
+                        </a>
+                        <a href="#">
+                            <img src="https://img.icons8.com/ios/50/null/log.png" alt="Dashboard icon" />
+                            Logout
+                        </a>
+                    </div>
+                </details>
+
+                <details>
+                    <summary>
+                        <img src="https://img.icons8.com/ios/50/null/settings.png" alt="User icon" />
+                        Settings
+                    </summary>
+                    <div class="container-div">
+                        <a href="#">
+                            <img src="https://img.icons8.com/ios/50/null/settings.png" alt="Dashboard icon" />
+                            Change Password
+                        </a>
+                        <a href="#">
+                            <img src="https://img.icons8.com/ios/50/null/settings.png" alt="Dashboard icon" />
+                            Edit Profile
+                        </a>
+                        <a href="#">
+                            <img src="https://img.icons8.com/ios/50/null/settings.png" alt="Dashboard icon" />
+                            Notification Settings
+                        </a>
+                        <a href="#">
+                            <img src="https://img.icons8.com/ios/50/null/settings.png" alt="Dashboard icon" />
+                            Logout
+                        </a>
+                    </div>
+                </details>
+            </div>
+
+            <div class="main">
+                {change 
+                ? <p>DashBoard</p>
+                :   <Lisst />
+
+                }
+            
+            </div>
+
+            <script src="script.js"></script>
         </div>
 
-        <div class="main">
-            {change 
-            ?   <AdminnAV />
-            :   <p>trefe</p>
+    )
+}
 
+
+function Lisst() {
+    const [array, setArray] = useState([]);
+    const local = localhost
+
+    useEffect(() => {
+        fetch('https://' + local + '/boitier')
+            .then(response => response.json())
+            .then(data => setArray(data))
+
+    }, [])
+
+    return (
+        <div>          
+           <form>
+            <label>designation</label>
+            <input type='text'></input>
+            <label>marque</label>
+            <input type='text'></input>
+            <label>image</label>
+            <input type='text'></input>
+            <button type='submit'>Ajouter</button>
+           </form>
+            {
+                array.map((itme) => {
+                    return (
+                        <div  key={itme.id}>
+                            <span> 
+                            <p>{itme.id} | {itme.designation} | {itme.marque}</p> 
+                                <button>Edit</button>
+                               <button>Supprimer</button>
+                            </span>
+                            {/* <img src={itme.imageUrl} alt={itme.designation} /> */}
+
+                        </div>
+                    )
+                })
             }
-        
         </div>
 
-        <script src="script.js"></script>
-    </div>
-
-  )
+    )
 }
 
 export default Admin
--- client/src/App/Admin/NavAdmin.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function NavAdmin() {
-  return (
-    <div>NavAdmin</div>
-  )
-}
-
-export default NavAdmin
