299c7a246a0ceef1fa5bd42606cd84fb53ec7da7
Tony <tony.brechard@epitech.eu>
2024-08-05 15:07:52 +0200

    [ADD] - On commence letape 2

--- client/src/App/user/Panier.js
+++ client/src/App/user/Panier.js
@@ -10,8 +10,12 @@ function Panier() {
     const [value, setValue] = useState([]);
     const [prixtotal, setPriceTotal] = useState([]);
     const [code, setCode] = useState('');
+    const [pays, setPays] = useState([]);
+    const [paysuser, setPaysUser] = useState('');
+    const [message, setMessage] = useState('');
     const [promo, setPromo] = useState(false);
     const navigate = useNavigate();
+
     useEffect(() => {
         fetch("https://localhost:8000/panier/" + loginUser.id)
             .then(reponse => reponse.json())
@@ -24,27 +28,85 @@ function Panier() {
             })
             .catch(erreur => console.error('Erreur: ', erreur));
     }, []);
-    const addCode = (code) => {
-        if (promo === false ) {
-            fetch("https://localhost:8000/code/" + code)
+
+    useEffect(() => {
+        fetch("https://epicareer.epidoc.eu/api/countries")
             .then(reponse => reponse.json())
             .then(data => {
-            
-                if (data.utilisations === 0) {
-                    setCode('')
-                    console.log('code promo fini')
-                    return
-                } else {
-                    setPriceTotal(prixtotal * (1 - data.promotion / 100))
-                    setCode('')
-                    CodeUtiliser()
-                    setPromo(true)
-                    return
-                }
+                setPays(data.data)
+
             })
             .catch(erreur => console.error('Erreur: ', erreur));
+    }, []);
+
+const PaysUser = (e) => {
+    setPaysUser(e.target.value)
+}
+    const SendColis = () => {
+        const userInfos = {
+            from: "France",
+            to: paysuser,
+            package: [
+                {
+                    content: "Books",
+                    weight: 1.5,
+                    width: 30,
+                    height: 15,
+                    length: 10
+                }
+            ],
+            currency: "EUR"
+        };
+
+        fetch("https://epicareer.epidoc.eu/api/package/estimate", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(userInfos),
+        })
+
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                if (data.success) {
+                    setMessage(data.data.travelTime);
+                    console.log(data.data)
+                    setPriceTotal(prixtotal + data.data.price.total)
+                }
+            })
+            .catch(error => {
+                console.error('Erreur:', error);
+            });
+
+
+
+    }
+
+
+
+    const addCode = (code) => {
+        if (promo === false) {
+            fetch("https://localhost:8000/code/" + code)
+                .then(reponse => reponse.json())
+                .then(data => {
+
+                    if (data.utilisations === 0) {
+                        setCode('')
+                        console.log('code promo fini')
+                        return
+                    } else {
+                        setPriceTotal(prixtotal * (1 - data.promotion / 100))
+                        setCode('')
+                        CodeUtiliser()
+                        setPromo(true)
+                        return
+                    }
+                })
+                .catch(erreur => console.error('Erreur: ', erreur));
         }
-       
+
     }
 
 
@@ -55,7 +117,7 @@ function Panier() {
             headers: {
                 'Content-Type': 'application/json',
             },
-           
+
         })
             .then(response => response.json())
             .then(data => console.log(data))
@@ -65,11 +127,11 @@ function Panier() {
 
     const AddProduit = (id, stock, quantity) => {
 
-        if (stock - 1 >= quantity) { 
+        if (stock - 1 >= quantity) {
 
             const Login = localStorage.getItem('users');
             const loginUser = JSON.parse(Login);
-    
+
             const userInfos = {
                 id_produit: id,
                 id_user: loginUser.id,
@@ -81,7 +143,7 @@ function Panier() {
                 },
                 body: JSON.stringify(userInfos),
             })
-    
+
                 .then(response => {
                     response.json();
                     window.location.reload()
@@ -92,6 +154,8 @@ function Panier() {
         }
 
     }
+
+
 
     const DeleteProduit = (id) => {
         const Login = localStorage.getItem('users');
@@ -119,40 +183,52 @@ function Panier() {
                 console.error('Erreur:', error);
             });
     }
-return (
-    <div>
-    <Nav />
-    <div className='panier-container'>
-        <h1 className="panier-title">Panier</h1>
-        <p className="panier-total">Prix total : {prixtotal}€</p>
-        <div className="promo-container">
-            <input 
-                type='text' 
-                value={code} 
-                onChange={(e) => setCode(e.target.value)} 
-                placeholder="CODE PROMO" 
-                className="promo-input" 
-            />
-            <button onClick={() => addCode(code)} className="promo-button">APPLIQUER</button>
+    return (
+        <div>
+            <Nav />
+            <div className='panier-container'>
+                <h1 className="panier-title">Panier</h1>
+                {message}
+                <p onClick={() => SendColis()}>Send</p>
+
+                <select onChange={(e) => PaysUser(e)}>
+                    <option value=''>Toutes les pays</option>
+                    {pays.map((country, index) => (
+                        <option key={index} value={country} >
+                            <p value={country}>{country}</p>
+                        </option>
+                    ))}
+                </select>
+
+                <p className="panier-total">Prix total : {prixtotal}€</p>
+                <div className="promo-container">
+                    <input
+                        type='text'
+                        value={code}
+                        onChange={(e) => setCode(e.target.value)}
+                        placeholder="CODE PROMO"
+                        className="promo-input"
+                    />
+                    <button onClick={() => addCode(code)} className="promo-button">APPLIQUER</button>
+                </div>
+                <ul className="panier-items">
+                    {value.map(item => (
+                        <li key={item.id} className="panier-item">
+                            <img src={item.image} alt={item.name} className="item-image" />
+                            <span className="item-info">
+                                <button onClick={() => DeleteProduit(item.id)} className="item-button">-</button>
+                                <button className="item-quantity">{item.quantity}</button>
+                                <button onClick={() => AddProduit(item.id)} className="item-button">+</button>
+                                <span className="item-details">
+                                    {item.name} - {(item.prix * (1 - item.promo / 100) * item.quantity)}€ | x1 {item.prix * (1 - item.promo / 100)}€
+                                </span>
+                            </span>
+                        </li>
+                    ))}
+                </ul>
+                <button className="checkout-button">Acheter</button>
+            </div>
         </div>
-        <ul className="panier-items">
-            {value.map(item => (
-                <li key={item.id} className="panier-item">
-                    <img src={item.image} alt={item.name} className="item-image" />
-                    <span className="item-info">
-                        <button onClick={() => DeleteProduit(item.id)} className="item-button">-</button>
-                        <button className="item-quantity">{item.quantity}</button>
-                        <button onClick={() => AddProduit(item.id)} className="item-button">+</button>
-                        <span className="item-details">
-                            {item.name} - {(item.prix * (1 - item.promo / 100) * item.quantity)}€ | x1 {item.prix * (1 - item.promo / 100)}€
-                        </span>
-                    </span>
-                </li>
-            ))}
-        </ul>
-        <button className="checkout-button">Acheter</button>
-    </div>
-</div>
 
     );
 }
