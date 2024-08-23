2d2116fba1242a92c669e71170c9f3af3827d996
Tony <tony.brechard@epitech.eu>
2024-08-23 11:37:50 +0200

    [UPDATE] - Fichier SQL

--- client/src/App/produits/Algo.js
+++ client/src/App/produits/Algo.js
@@ -5,11 +5,13 @@ function Algo() {
   const [produits, setProduits] = useState([]);
   const [boitiers, setBoitiers] = useState([]);
   const [carte, setCarte] = useState([]);
+  const [cpu, setCPU] = useState([]);
 
   const [panierWait, setPanierWait] = useState([]);
   const [step, setStep] = useState(1);
 
   const [taille, setTaille] = useState('');
+  const [type, setType] = useState('');
 
   useEffect(() => {
     fetch("https://localhost:8000/produits")
@@ -31,6 +33,16 @@ function Algo() {
         .catch(error => console.error('Erreur: ', error));
     }
 
+    const NextStepCPU = (type) => {
+      fetch("https://localhost:8000/produits")
+        .then(response => response.json())
+        .then(data => {
+          const filteBoitiers = data.filter(element => element.id_categorie === 4 && element.typec === type);
+          setCPU(filteBoitiers);
+        })
+        .catch(error => console.error('Erreur: ', error));
+    }
+
   const Choose = (e) => {
     const porduitAdd = JSON.parse(e.target.value)
     setPanierWait({
@@ -42,10 +54,27 @@ function Algo() {
     NextStep(porduitAdd.taille)
   }
 
+  const Choose2 = (e) => {
+    const porduitAdd = JSON.parse(e.target.value)
+    setPanierWait({
+      ...panierWait,
+      [e.target.name]: porduitAdd
+    });
+    setType(porduitAdd.typec)
+    setStep(3)
+    NextStepCPU(porduitAdd.typec)
+  }
+console.log(panierWait)
+
   return (
     <div>
       <Nav />
       <div>
+         {panierWait.map((boitier, index) => (
+              <li key={index}>
+                {boitier.name} 
+              </li>
+            ))}
         {step === 1 && (
           <ul>
             <h2>Liste des Boîtiers Disponibles :</h2>
@@ -58,10 +87,20 @@ function Algo() {
         )}
          {step === 2 && (
           <ul>
-            <h2>Liste des Boîtiers Disponibles :</h2>
+            <h2>Liste des Carte Mere Disponibles :</h2>
             {carte.map((boitier, index) => (
               <li key={index}>
-                {boitier.name} (Format: {boitier.taille}) <button value={JSON.stringify(boitier)} name={boitier.id} onClick={(e) => Choose(e)}>+</button>
+                {boitier.name} (Format: {boitier.typec}) <button value={JSON.stringify(boitier)} name={boitier.id} onClick={(e) => Choose2(e)}>+</button>
+              </li>
+            ))}
+          </ul>
+        )}
+         {step === 3 && (
+          <ul>
+            <h2>Liste des Carte Mere Disponibles :</h2>
+            {cpu.map((boitier, index) => (
+              <li key={index}>
+                {boitier.name} (Format: {boitier.type}) <button value={JSON.stringify(boitier)} name={boitier.id} onClick={(e) => Choose2(e)}>+</button>
               </li>
             ))}
           </ul>
