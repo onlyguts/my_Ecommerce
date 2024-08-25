aa6281e6262f666e1d3c69cdd21f012c1c2a2afd
Tony <tony.brechard@epitech.eu>
2024-08-25 15:56:25 +0200

    [UPDATE] - Algo Build Your Pc

--- client/src/App/produits/Algo.js
+++ client/src/App/produits/Algo.js
@@ -6,12 +6,14 @@ function Algo() {
   const [boitiers, setBoitiers] = useState([]);
   const [carte, setCarte] = useState([]);
   const [cpu, setCPU] = useState([]);
+  const [ram, setRAM] = useState([]);
 
   const [panierWait, setPanierWait] = useState([]);
-  const [step, setStep] = useState(1);
+  const [step, setStep] = useState(0);
 
   const [taille, setTaille] = useState('');
   const [type, setType] = useState('');
+  const [ramType, setRamType] = useState('');
 
   useEffect(() => {
     fetch("https://localhost:8000/produits")
@@ -23,25 +25,36 @@ function Algo() {
       .catch(error => console.error('Erreur: ', error));
   }, []);
 
-    const NextStep = (tailles) => {
-      fetch("https://localhost:8000/produits")
-        .then(response => response.json())
-        .then(data => {
-          const filteBoitiers = data.filter(element => element.id_categorie === 2 && element.taille === tailles);
-          setCarte(filteBoitiers);
-        })
-        .catch(error => console.error('Erreur: ', error));
-    }
+  const NextStep = (tailles) => {
+    fetch("https://localhost:8000/produits")
+      .then(response => response.json())
+      .then(data => {
+        const filteBoitiers = data.filter(element => element.id_categorie === 2 && element.taille === tailles);
+        setCarte(filteBoitiers);
+      })
+      .catch(error => console.error('Erreur: ', error));
+  }
 
-    const NextStepCPU = (type) => {
-      fetch("https://localhost:8000/produits")
-        .then(response => response.json())
-        .then(data => {
-          const filteBoitiers = data.filter(element => element.id_categorie === 4 && element.typec === type);
-          setCPU(filteBoitiers);
-        })
-        .catch(error => console.error('Erreur: ', error));
-    }
+  const NextStepCPU = (type) => {
+    fetch("https://localhost:8000/produits")
+      .then(response => response.json())
+      .then(data => {
+        const filteBoitiers = data.filter(element => element.id_categorie === 4 && element.typec === type);
+        setCPU(filteBoitiers);
+      })
+      .catch(error => console.error('Erreur: ', error));
+  }
+
+  const NextStepRAM = (socket) => {
+    fetch("https://localhost:8000/produits")
+      .then(response => response.json())
+      .then(data => {
+        const filteBoitiers = data.filter(element => element.id_categorie === 5 && element.socket === ramType);
+        setRAM(filteBoitiers);
+      })
+      .catch(error => console.error('Erreur: ', error));
+  }
+
 
   const Choose = (e) => {
     const porduitAdd = JSON.parse(e.target.value)
@@ -61,20 +74,39 @@ function Algo() {
       [e.target.name]: porduitAdd
     });
     setType(porduitAdd.typec)
+    setRamType(porduitAdd.socket)
     setStep(3)
     NextStepCPU(porduitAdd.typec)
+    
   }
-console.log(panierWait)
+
+  const Choose3 = (e) => {
+    const porduitAdd = JSON.parse(e.target.value)
+    setPanierWait({
+      ...panierWait,
+      [e.target.name]: porduitAdd
+    });
+
+    setStep(4)
+    NextStepRAM(porduitAdd.socket)
+  }
+  
+  const StartPc = () => {
+    setStep(1)
+  }
 
   return (
     <div>
       <Nav />
       <div>
-         {Object.values(panierWait).map((boitier, index) => (
-              <li key={index}>
-                {boitier.name} 
-              </li>
-            ))}
+        {Object.values(panierWait).map((boitier, index) => (
+          <li key={index}>
+            {boitier.name}
+          </li>
+        ))}
+        {step === 0 && (
+          <button onClick={() => StartPc()}>Commencer</button>
+        )}
         {step === 1 && (
           <ul>
             <h2>Liste des Boîtiers Disponibles :</h2>
@@ -85,7 +117,7 @@ console.log(panierWait)
             ))}
           </ul>
         )}
-         {step === 2 && (
+        {step === 2 && (
           <ul>
             <h2>Liste des Carte Mere Disponibles :</h2>
             {carte.map((boitier, index) => (
@@ -95,12 +127,22 @@ console.log(panierWait)
             ))}
           </ul>
         )}
-         {step === 3 && (
+        {step === 3 && (
           <ul>
             <h2>Liste des CPU Disponibles :</h2>
             {cpu.map((boitier, index) => (
               <li key={index}>
-                {boitier.name} (Format: {boitier.type}) <button value={JSON.stringify(boitier)} name={boitier.id} onClick={(e) => Choose2(e)}>+</button>
+                {boitier.name} (Format: {boitier.type}) <button value={JSON.stringify(boitier)} name={boitier.id} onClick={(e) => Choose3(e)}>+</button>
+              </li>
+            ))}
+          </ul>
+        )}
+           {step === 4 && (
+          <ul>
+            <h2>Liste des CPU Disponibles :</h2>
+            {ram.map((boitier, index) => (
+              <li key={index}>
+                {boitier.name} (Format: {boitier.type}) <button value={JSON.stringify(boitier)} name={boitier.id} onClick={(e) => Choose3(e)}>+</button>
               </li>
             ))}
           </ul>
